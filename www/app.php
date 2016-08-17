11a2a0f515aab2e333c2c108799ebce0ede86517
matthes <leuffen@continue.de>
2016-08-17 18:50:49 +0200

    Running with annotations

--- www/app.php
+++ www/app.php
@@ -11,9 +11,12 @@
 
     ini_set("display_errors", 1);
 
+    // Aktivieren der Html-Sauberen Exception Darstellung
     \Gismo\Component\PhpFoundation\Helper\ErrorHandler::UseHttpErrorHandler();
 
+    // Request aus Environment bauen
     $request = \Gismo\Component\HttpFoundation\Request\RequestFactory::BuildFromEnv();
 
+    // App Laden und ausführen.
     $app = new \Gismo\TutorialBasic1\App\HomepageApp();
     $app->run($request);
